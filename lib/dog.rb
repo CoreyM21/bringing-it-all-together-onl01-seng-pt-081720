547ec44d74530501c67c4f8735c330ae3824d179
Corey Martin <coreyjmartin2@gmail.com>
2020-11-02 03:42:15 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -29,7 +29,20 @@ class Dog
     DB[:conn].execute(sql)
   end
   
+  def self.new_from_db(row)
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
+    new_dog
+  end
   
+  def self.find_by_name(name)
+    sql = <<-SQL 
+      SELECT * FROM dogs
+      WHERE name = ?
+    SQL
+  
+     result = DB[:conn].execute(sql, name)[0]
+     Dog.new(id: result[0], name: result[1], breed: result[2])
+  end
     
     
   
